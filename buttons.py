c2bcdafb20b9ef4782a6a08b054433da82768e84
Gusta2307 <71906843+Gusta2307@users.noreply.github.com>
2021-06-02 06:33:17 -0400

    Add files via upload

--- buttons.py
+++ buttons.py
@@ -1,3 +1,13 @@
+import logging
+import telegram
+from telegram.ext import Updater, MessageHandler, CommandHandler, InlineQueryHandler, Filters, ConversationHandler, CallbackQueryHandler
+from telegram import InlineQueryResultArticle, InputTextMessageContent, User
+
+logging.basicConfig(
+    level = logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger()
 
 def build_menu(buttons, n_cols, header_buttons=None, header_buttons1=None, footer_buttons=None):
     menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
@@ -10,3 +20,9 @@ def build_menu(buttons, n_cols, header_buttons=None, header_buttons1=None, foote
         menu.append(footer_buttons)
 
     return menu
+
+def cancel(update, context):
+    logger.info(f"El usuario {update.effective_user['first_name']}, cancel")
+    update.callback_query.message.delete()
+    return ConversationHandler.END
+    
