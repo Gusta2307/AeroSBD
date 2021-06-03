39894ca699992115e0774a1f6e70f29e2a70a32c
Gusta2307 <71906843+Gusta2307@users.noreply.github.com>
2021-06-03 08:29:08 -0400

    Add files via upload

--- employee_salesManager.py
+++ employee_salesManager.py
@@ -270,7 +270,7 @@ def save_passeger_salesManager_callback_query(update, context): #REVISAR BIEN
     elif query == "no" or query == "atras":
         msg = into_data_pasajero_client_menu(context)
         msg[0] = data_passeger_salesManager_refresh(msg[0], context)
-        update.callback_query.message.bot.edit_text(text=msg[0], parse_mode = 'Markdown', reply_markup=msg[1])
+        update.callback_query.message.edit_text(text=msg[0], parse_mode = 'Markdown', reply_markup=msg[1])
         return EMPLOYEE_SALESMANAGER_INTO_DATA_PASS
     else:
         return cancel(update, context)
