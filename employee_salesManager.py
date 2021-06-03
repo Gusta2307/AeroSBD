7a0ff0f246bf2a883cd04da8ec819543f7493543
Gusta2307 <71906843+Gusta2307@users.noreply.github.com>
2021-06-03 08:22:44 -0400

    Add files via upload

--- employee_salesManager.py
+++ employee_salesManager.py
@@ -146,7 +146,7 @@ def save_data_salesManager_callback_query(update, context):
         msg = into_data_owner_client_menu(context)
         msg[0] = data_owner_salesManager_refresh(msg[0], context)
         update.callback_query.message.edit_text(text=msg[0], parse_mode = 'Markdown', reply_markup=msg[1])
-        return EMPLOYEE_SALESMANAGER_SELLPASAJE
+        return EMPLOYEE_SALESMANAGER_INTO_DATA_OWNER
     else:
         return cancel(update, context)
 
